6003a512982752c2cb55be66dcf08f79e2acf5ea
Nguyễn Việt Quốc An <40015342+cloodei@users.noreply.github.com>
2025-04-25 22:58:41 +0700

    Testing crates and mods?

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,7 @@
-use std::time;
-use rand::random_range;
-
 pub mod sorts;
 pub mod utils;
-pub mod radix;
-
-const SIZE: usize = 1_000_000;
+pub mod run_sorts;
 
 fn main() {
-    let mut arr = Vec::with_capacity(SIZE);
-    for _ in 0..SIZE {
-        arr.push(random_range(-524_288..1_048_576));
-    }
-
-    let mut arr2 = arr.clone();
-    let mut start = time::Instant::now();
-    arr2.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    let mut elapsed = start.elapsed();
-    println!("std::sort()  time: {:.2?} ms | {}", elapsed.as_micros() as f64 / 1000.0, utils::is_sorted_strict(arr.as_slice(), arr2.as_slice()));
-
-    arr2 = arr.clone();
-    start = time::Instant::now();
-    sorts::heap_sort(arr2.as_mut_slice());
-    elapsed = start.elapsed();
-    println!("heap_sort()  time: {:.2?} ms | {}", elapsed.as_micros() as f64 / 1000.0, utils::is_sorted_strict(arr.as_slice(), arr2.as_slice()));
-
-    arr2 = arr.clone();
-    start = time::Instant::now();
-    sorts::quick_sort(arr2.as_mut_slice());
-    elapsed = start.elapsed();
-    println!("quick_sort() time: {:.2?} ms | {}", elapsed.as_micros() as f64 / 1000.0, utils::is_sorted_strict(arr.as_slice(), arr2.as_slice()));
-
-    arr2 = arr.clone();
-    start = time::Instant::now();
-    sorts::merge_sort(arr2.as_mut_slice());
-    elapsed = start.elapsed();
-    println!("merge_sort() time: {:.2?} ms | {}", elapsed.as_micros() as f64 / 1000.0, utils::is_sorted_strict(arr.as_slice(), arr2.as_slice()));
+    run_sorts::run::run();
 }
