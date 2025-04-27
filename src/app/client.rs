fb2412292661479497d837c5aaa23cd9db6096fa
Nguyễn Việt Quốc An <40015342+cloodei@users.noreply.github.com>
2025-04-27 11:58:56 +0700

    Wrapping up

--- src/app/client.rs
+++ src/app/client.rs
@@ -2,8 +2,8 @@
 
 pub async fn Client() {
     let mut client = mini_redis::client::connect("127.0.0.1:6379").await.unwrap();
-    client.set("but", "nobody came...".into()).await.unwrap();
+    client.set("the phone rang", "but nobody came...".into()).await.unwrap();
 
-    let stuff = client.get("but").await.unwrap();
-    println!("Got: {:?}", stuff);
+    let stuff = client.get("the phone rang").await.unwrap();
+    println!("Got: {:?}", stuff.unwrap());
 }
